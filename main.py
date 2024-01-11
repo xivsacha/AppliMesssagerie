a46010c32e4275469bc969c02b1e2c97b0e2a9e3
Romain.jhr <romain.jahier@edu.esiee-it.fr>
2024-01-11 11:42:49 +0100

    first commit feature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from flask import Flask, redirect, request, render_template, url_for
-from models import db, Message
-from datetime import datetime
+from flask import Flask, redirect, request, render_template, url_for, jsonify
+from models import Group, db, Message
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messages.db'
@@ -13,18 +12,42 @@ def index():
         pseudo = request.form['pseudo']
         ip_address = request.remote_addr
         message_content = request.form['content']
+        group_id = request.form.get('group')
 
-        message = Message(pseudo=pseudo, content=message_content, ip_address=ip_address)
+        message = Message(pseudo=pseudo, content=message_content, ip_address=ip_address, group_id=group_id)
         db.session.add(message)
         db.session.commit()
 
         return redirect(url_for('index'))
-    return render_template('index.html')
 
-@app.route('/messages')
-def messages():
-    all_messages = Message.query.order_by(Message.timestamp.asc()).all()
-    return render_template('messages.html', messages=all_messages)
+    all_groups = Group.query.all()
+    conversation_id = request.args.get('conversation_id')
+
+    if conversation_id:
+        messages = Message.query.filter_by(group_id=conversation_id).order_by(Message.timestamp.asc()).all()
+    else:
+        messages = []
+
+    return render_template('index.html', messages=messages, groups=all_groups, conversation_id=conversation_id)
+
+@app.route('/group', methods=['GET', 'POST'])
+def group():
+    if request.method == 'POST':
+        group_name = request.form['group_name']
+
+        new_group = Group(name=group_name)
+        db.session.add(new_group)
+        db.session.commit()
+
+    all_groups = Group.query.all()
+
+    return render_template('group.html', groups=all_groups)
+
+@app.route('/get_groups', methods=['GET'])
+def get_groups():
+    all_groups = Group.query.all()
+    groups_data = [{'id': group.id, 'name': group.name} for group in all_groups]
+    return jsonify(groups_data)
 
 if __name__ == '__main__':
     with app.app_context():
