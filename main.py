a6090e8f30b03a5bf4494169666559e91cfd83a2
Romain.jhr <romain.jahier@edu.esiee-it.fr>
2024-01-11 14:07:18 +0100

    fix conversation par group

--- main.py
+++ main.py
@@ -10,23 +10,21 @@ db.init_app(app)
 def index():
     if request.method == 'POST':
         pseudo = request.form['pseudo']
-        ip_address = request.remote_addr
         message_content = request.form['content']
-        group_id = request.form.get('group')
+        conversation_id = request.form['group']
 
-        message = Message(pseudo=pseudo, content=message_content, ip_address=ip_address, group_id=group_id)
+        message = Message(pseudo=pseudo, content=message_content, group_id=conversation_id)
         db.session.add(message)
         db.session.commit()
 
-        return redirect(url_for('index'))
+        return redirect(url_for('index', group=conversation_id))
 
     all_groups = Group.query.all()
-    conversation_id = request.args.get('conversation_id')
+    conversation_id = request.args.get('group')
 
+    messages = []
     if conversation_id:
         messages = Message.query.filter_by(group_id=conversation_id).order_by(Message.timestamp.asc()).all()
-    else:
-        messages = []
 
     return render_template('index.html', messages=messages, groups=all_groups, conversation_id=conversation_id)
 
