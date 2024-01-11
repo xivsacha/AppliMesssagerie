51aac786fe3cb6f554e407ac51807d782f383f35
Romain.jhr <romain.jahier@edu.esiee-it.fr>
2024-01-11 09:19:20 +0100

    groupe

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
 from flask import Flask, redirect, request, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
+from config import db
+from models import Message
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messages.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+db.init_app(app)
 
-class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-    pseudo = db.Column(db.String(50))
-    ip_address = db.Column(db.String(15))
-
-    def __repr__(self):
-        return f'<Message {self.id}>'
-    
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -30,12 +21,6 @@ def index():
 
         return redirect(url_for('index'))
     return render_template('index.html')
-
-
-@app.route('/messages')
-def messages():
-    all_messages = Message.query.order_by(Message.timestamp.asc()).all()
-    return render_template('messages.html', messages=all_messages)
 
 if __name__ == '__main__':
     with app.app_context():
---
+++ models.py
@@ -0,0 +1,23 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from config import db
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    pseudo = db.Column(db.String(50))
+    ip_address = db.Column(db.String(15))
+
+    def __repr__(self):
+        return f'<Message {self.id}>'
+
+class Group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    members = db.relationship('Member', backref='group', lazy=True)
+
+class Member(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    group_id = db.Column(db.Integer, db.ForeignKey('group.id'), nullable=False)
+    ip_address = db.Column(db.String(15), nullable=False)
