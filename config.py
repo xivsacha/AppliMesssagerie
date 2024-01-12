fbb0dca812ece4c99fe99bf014e79f48d683c7b3
Sacha <sacha.harel@gmail.com>
2024-01-12 14:13:55 +0100

    ip update

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'une-cle-secrete-tres-difficile'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flaskuser:password@192.168.70.249/appli_messagerie'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flaskuser:password@10.57.32.9/appli_messagerie'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
